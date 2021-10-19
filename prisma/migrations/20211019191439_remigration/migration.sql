6745f0bde0ef9114d04983b5c04a02b8b3fdbfaf
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2021-10-19 16:16:42 -0300

    Alter migration test

--- prisma/migrations/20211019191439_remigration/migration.sql
+++ prisma/migrations/20211019191439_remigration/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "email" TEXT
+);
+
+-- CreateTable
 CREATE TABLE "messages" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "text" TEXT NOT NULL,
