de0cae28b504797e4d382c96f3a129b8907187c5
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2021-10-19 21:22:11 -0300

    Build on

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        legacy: true,
+      },
+    ],
+    "transform-node-env-inline",
+  ],
+};
